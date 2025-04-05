cfc3339d16107b48b208812db17b88ae1b698769
Virender Chauhan <bikuchauhan786+github@gmail.com>
2025-04-05 17:10:34 +0530

    package.json updated

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { useMemo } from "react";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000"; 
 
 const useAxios = () => {
   const instance = useMemo(() => {
