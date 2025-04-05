2a584ba9fe88f1c9070302a73a4b37b6a6b4f58b
Virender Chauhan <bikuchauhan786+github@gmail.com>
2025-04-05 18:03:46 +0530

    updated axios hook for resolving cookie error

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -2,26 +2,25 @@
 import axios from "axios";
 import { useMemo } from "react";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000"; 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
 const useAxios = () => {
   const instance = useMemo(() => {
     const client = axios.create({
       baseURL: BASE_URL,
+      withCredentials: true,
       headers: {
         "Content-Type": "application/json",
-        
-      },withCredentials: true, 
+      },
     });
 
-   
     client.interceptors.response.use(
       (response) => response,
       (error) => {
         if (error.response?.status === 401) {
           console.warn("Unauthorized, redirecting...");
           localStorage.removeItem("schedulerUserName");
-          window.location.href = "/signin"; 
+          window.location.href = "/signin";
         }
         return Promise.reject(error);
       },
